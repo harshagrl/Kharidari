2f09ac42306c7c59d788d3f8e8b778509433c51b
harsh <harshkr.agrl@gmail.com>
2025-12-17 15:21:25 +0530

    fix vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_URL || "http://localhost:5000",
-        changeOrigin: true,
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:5000",
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  });
+};
